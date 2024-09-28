b44a64ebf7cbe2ca99aeaa834a980987b5da4500
pi-maaster-77 <pimaaster1337@gmail.com>
2024-09-28 00:32:25 -0300

    index.html ahora funciona

---
+++ static/scripts/crear.js
@@ -0,0 +1,29 @@
+document.getElementById('crear').addEventListener('submit', async function(e) {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('title', document.getElementById('title').value);
+    formData.append('image', document.getElementById('image').files[0]);
+    //formData.append('token', localStorage.getItem("userToken"));
+    try {
+        const response = await fetch('/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            document.getElementById('message').innerHTML = `<p style="color: green;">${result.message}</p>`;
+            setTimeout(function() {
+              window.location.href = "/"
+            }, 2000);
+
+        } else {
+            document.getElementById('message').innerHTML = `<p style="color: red;">Error: ${result.error}</p>`;
+        }
+    } catch (error) {
+        console.error('Error al subir el archivo:', error);
+        document.getElementById('message').innerHTML = `<p style="color: red;">Error al subir el archivo</p>`;
+    }
+});
